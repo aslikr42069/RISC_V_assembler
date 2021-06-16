e613dc39fbfc360c833be56d5627855c30ccaa35
aslikr211234 <arturo211234@gmail.com>
2021-06-15 23:28:45 -0500

    Update main.c

--- main.c
+++ main.c
@@ -18,7 +18,7 @@ size_t count_words(const char *input, size_t string_length){
 }
 
 int main(){
- const char *input = "addi x1, x0, 100\n";
+ const char *input = "addi x1, x0, 100\nxori";
  size_t word_count = 0;
  size_t string_length = strlen(input);
  
@@ -73,6 +73,7 @@ int main(){
    line_count++;
   }
  }
+
  size_t line[line_count]; // Array telling us the index of the string in which the newlines lie
  line[0] = 0; // Just saying that the first line starts at the first character
  if(line_count > 1){
@@ -85,5 +86,60 @@ int main(){
   }
  }
  
- printf("Line Count: %li\n", line_count); // Test
+ size_t instruction_count = 0;
+ size_t word_length = 0;
+ size_t instruction_length = 0;
+ instruction_count = sizeof(instruction_string) / sizeof(instruction_string[0]); 
+ index = 0;
+ size_t instructions_in_code = 0;
+
+ for(size_t i = 0; i < word_count; i++){ /* Code for getting the amount of instructions in the source code */
+  word_length = word_end[i] - word_start[i];
+  for(size_t j = 0; j < instruction_count; j++){
+   instruction_length = strnlen(instruction_string[j], 6);
+   if(word_length == instruction_length){
+    index = word_start[i];
+    if(strncmp(input + index, instruction_string[j], word_length) == 0){
+     instructions_in_code++;
+     break;
+    }
+   }
+  }
+ }
+
+ size_t instruction[instructions_in_code];
+ size_t index_of_instructions = 0;
+ 
+ for(size_t i = 0; i < word_count; i++){ /* Code for getting where in the source code the instructions are located */
+  word_length = word_end[i] - word_start[i];
+  for(size_t j = 0; j < instruction_count; j++){
+   instruction_length = strnlen(instruction_string[j], 6);
+   if(word_length == instruction_length){
+    index = word_start[i];
+    if(strncmp(input + index, instruction_string[j], word_length) == 0){
+     instruction[index_of_instructions] = i;
+     index_of_instructions++;
+     break;
+    }
+   }
+  }
+ }
+ 
+ size_t number_count = 0;
+ for(size_t i = 0; i < word_count; i++){ /* Code for getting amount of numbers in source code */
+  if(input[word_end[i] - 1] >= '0' && input[word_end[i] - 1] <= '9'){
+   number_count++;
+  }
+ }
+ 
+ size_t number[number_count];
+ index = 0;
+ for(size_t i = 0; i < word_count; i++){ /* Code for getting position of numbers in source code */
+  if(input[word_end[i] - 1] >= '0' && input[word_end[i] - 1] <= '9'){
+   number[index] = i;
+   index++;
+  }
+ }
+
+ printf("Amount of Numbers = %li\n", number_count); // Test
 }
