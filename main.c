ebc51bb21fb8f864d6c3f228f4bb8c5ed315f49f
aslikr211234 <arturo211234@gmail.com>
2021-06-16 18:39:32 -0500

    Update main.c

--- main.c
+++ main.c
@@ -4,11 +4,20 @@
 #include "instructions.h"
 
 
-size_t count_words(const char *input, size_t string_length){
+size_t count_words(const char *input, size_t string_length, size_t *comment_start, size_t *comment_end, size_t comment_count){
  size_t word_count = 0;
+ uint8_t not_comment = 1;
  for(size_t i = 0; i < string_length; i++){
+  not_comment = 1;
   if((input[i] >= '0' && input[i] <= '9') || (input[i] >= 'A' && input[i] <= 'Z') || (input[i] >= 'a' && input[i] <= 'z')){
-   word_count++;
+   for(size_t j = 0; j < comment_count; j++){
+    if((i >= comment_start[j]) && (i < comment_end[j])){
+     not_comment = 0;
+    }
+   }
+   if(not_comment == 1){
+    word_count++;
+   }
    while(((input[i] >= '0' && input[i] <= '9') || (input[i] >= 'A' && input[i] <= 'Z') || (input[i] >= 'a' && input[i] <= 'z')) && i < string_length){
     i++;
    }
@@ -18,27 +27,85 @@ size_t count_words(const char *input, size_t string_length){
 }
 
 int main(){
- const char *input = "addi x1, x0, 100\nxori";
+ const char *input = "addi x1, x0, 100 # Comment begin \nxori x1, x2, 100 # Another one\n addi x1, x2, 100 # One alst for good measure";
  size_t word_count = 0;
  size_t string_length = strlen(input);
- 
- word_count = count_words(input, string_length);
- size_t word_start[word_count];
- size_t word_end[word_count];
- 
  size_t index = 0;
- printf("Word Count: %li\n", word_count); // Test
 
- for(size_t i = 0; i < string_length; i++){ /* Code for getting indexes of the start of words */
-  if((input[i] >= '0' && input[i] <= '9') || (input[i] >= 'A' && input[i] <= 'Z') || (input[i] >= 'a' && input[i] <= 'z')){
-   word_start[index] = i;
+ size_t comment_count = 0;
+ for(size_t i = 0; i < string_length; i++){ /* Code for counting the number of comments */
+  if(input[i] == '#'){
+   comment_count++;
+  }
+ }
+
+ index = 0;
+ size_t comment_start[comment_count];
+ for(size_t i = 0; i < string_length; i++){ /* Code for getting where a comment starts */
+  if(input[i] == '#'){
+   comment_start[index] = i;
    index++;
-   while(((input[i] >= '0' && input[i] <= '9') || (input[i] >= 'A' && input[i] <= 'Z') || (input[i] >= 'a' && input[i] <= 'z')) && i < string_length){
-    i++;
+  }
+ }
+ size_t comment_end[comment_count];
+ 
+ size_t line_count = 1;
+ for(size_t i = 0; i < string_length; i++){
+  if(input[i] == '\n'){
+   line_count++;
+  }
+ }
+
+ size_t line[line_count]; // Array telling us the index of the string in which the newlines lie
+ line[0] = 0; // Just saying that the first line starts at the first character
+ if(line_count > 1){
+ index = 1;
+ for(size_t i = 0; i < string_length; i++){
+  if(input[i] == '\n'){
+   line[index] = i;
+   index++;
    }
   }
  }
+
  
+ for(size_t i = 0; i < comment_count; i++){ /* Code for getting where comments end */
+  index = comment_start[i];
+  while(input[index] != '\n' && index < string_length){ 
+   index++;
+  }
+  comment_end[i] = index; 
+ }
+ 
+ index = 0;
+ word_count = count_words(input, string_length, comment_start, comment_end, comment_count);
+ size_t word_start[word_count];
+ size_t word_end[word_count];
+ 
+ printf("Word Count: %li\n", word_count); // Test
+ printf("Comment Count: %li\n", comment_count);
+ //uint8_t not_comment = 1;
+
+ for(size_t i = 0; i < string_length; i++){ /* Code for getting indexes of the start of words */
+  for(size_t j = 0; j < comment_count; j++){
+   if((i >= comment_start[j]) && (i < comment_end[j])){
+    i++;
+   } else{
+    if((input[i] >= '0' && input[i] <= '9') || (input[i] >= 'A' && input[i] <= 'Z') || (input[i] >= 'a' && input[i] <= 'z')){ 
+     word_start[index] = i;
+     index++;
+     while(((input[i] >= '0' && input[i] <= '9') || (input[i] >= 'A' && input[i] <= 'Z') || (input[i] >= 'a' && input[i] <= 'z')) && i < string_length){
+      i++;
+     }
+    }
+   }
+  }
+ }
+ for(size_t i = 0; i < word_count; i++){
+  printf("Word Start %li: %li\n", i, word_start[i]);
+ }
+
+ index = 0;
  for(size_t i = 0; i < word_count; i++){ /* Code for getting indexes of the end of words */
   index = word_start[i];
   while(((input[index] >= '0' && input[index] <= '9') || (input[index] >= 'A' && input[index] <= 'Z') || (input[index] >= 'a' && input[index] <= 'z')) && index < string_length){
@@ -67,24 +134,6 @@ int main(){
   }
  }
  
- size_t line_count = 1;
- for(size_t i = 0; i < string_length; i++){
-  if(input[i] == '\n'){
-   line_count++;
-  }
- }
-
- size_t line[line_count]; // Array telling us the index of the string in which the newlines lie
- line[0] = 0; // Just saying that the first line starts at the first character
- if(line_count > 1){
- index = 1;
- for(size_t i = 0; i < string_length; i++){
-  if(input[i] == '\n'){
-   line[index] = i;
-   index++;
-   }
-  }
- }
  
  size_t instruction_count = 0;
  size_t word_length = 0;
