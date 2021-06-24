ec1e63ea45a35ed9d76de0a1a8a35ea9587ed792
aslikr42069 <arturo211234@gmail.com>
2021-06-24 02:10:25 -0500

    Add even more instructions

--- generate_machine_code.h
+++ generate_machine_code.h
@@ -155,28 +155,112 @@ uint32_t *generate_machine_code(char *input, size_t word_count, size_t *words[],
     current_instruction++;
     break;
    case 15:/* sb */
+    if(number[current_number + 1] > 31 || number[current_number + 2] > 31){
+     printf("Error: Line %li. Register provided is greater than registers available.\n", current_line);
+     exit(1);
+    }
+    machine_code[i] = 35 | ((number[current_number] & 0x1F) << 6) | (0x0 << 11) | ((number[current_number + 1] & 0x1F) << 14) | ((number[current_number + 2] & 0x1F) << 19) | ((number[current_number] & 0xFE0) << 19);
+    current_number += 3;
+    current_instruction++; 
     break;
    case 16:/* sh */
+    if(number[current_number + 1] > 31 || number[current_number + 2] > 31){
+     printf("Error: Line %li. Register provided is greater than registers available.\n", current_line);
+     exit(1);
+    }
+    machine_code[i] = 35 | ((number[current_number] & 0x1F) << 6) | (0x1 << 11) | ((number[current_number + 1] & 0x1F) << 14) | ((number[current_number + 2] & 0x1F) << 19) | ((number[current_number] & 0xFE0) << 19);
+    current_number += 3;
+    current_instruction++; 
     break;
    case 17:/* sw */
+    if(number[current_number + 1] > 31 || number[current_number + 2] > 31){
+     printf("Error: Line %li. Register provided is greater than registers available.\n", current_line);
+     exit(1);
+    }
+    machine_code[i] = 35 | ((number[current_number] & 0x1F) << 6) | (0x2 << 11) | ((number[current_number + 1] & 0x1F) << 14) | ((number[current_number + 2] & 0x1F) << 19) | ((number[current_number] & 0xFE0) << 19);
+    current_number += 3;
+    current_instruction++; 
     break;
    case 18:/* addi */
+    if(number[current_number + 1] > 31 || number[current_number + 2] > 31){
+     printf("Error: Line %li. Register provided is greater than registers available.\n", current_line);
+     exit(1);
+    }
+    machine_code[i] = 35 | ((number[current_number] & 0x1F) << 6) | (0x0 << 11) | ((number[current_number + 1] & 0x1F) << 14) | ((number[current_number + 2] & 0xFFF) << 19);
+    current_number += 3;
+    current_instruction++; 
     break;
    case 19:/* slti */
+    if(number[current_number + 1] > 31 || number[current_number + 2] > 31){
+     printf("Error: Line %li. Register provided is greater than registers available.\n", current_line);
+     exit(1);
+    }
+    machine_code[i] = 35 | ((number[current_number] & 0x1F) << 6) | (0x2 << 11) | ((number[current_number + 1] & 0x1F) << 14) | ((number[current_number + 2] & 0xFFF) << 19);
+    current_number += 3;
+    current_instruction++;
     break;
    case 20:/* sltiu */
+    if(number[current_number + 1] > 31 || number[current_number + 2] > 31){
+     printf("Error: Line %li. Register provided is greater than registers available.\n", current_line);
+     exit(1);
+    }
+    machine_code[i] = 35 | ((number[current_number] & 0x1F) << 6) | (0x3 << 11) | ((number[current_number + 1] & 0x1F) << 14) | ((number[current_number + 2] & 0xFFF) << 19);
+    current_number += 3;
+    current_instruction++;
     break;
    case 21:/* xori */
+    if(number[current_number + 1] > 31 || number[current_number + 2] > 31){
+     printf("Error: Line %li. Register provided is greater than registers available.\n", current_line);
+     exit(1);
+    }
+    machine_code[i] = 35 | ((number[current_number] & 0x1F) << 6) | (0x4 << 11) | ((number[current_number + 1] & 0x1F) << 14) | ((number[current_number + 2] & 0xFFF) << 19);
+    current_number += 3;
+    current_instruction++;
     break;
    case 22:/* ori */
+    if(number[current_number + 1] > 31 || number[current_number + 2] > 31){
+     printf("Error: Line %li. Register provided is greater than registers available.\n", current_line);
+     exit(1);
+    }
+    machine_code[i] = 35 | ((number[current_number] & 0x1F) << 6) | (0x6 << 11) | ((number[current_number + 1] & 0x1F) << 14) | ((number[current_number + 2] & 0xFFF) << 19);
+    current_number += 3;
+    current_instruction++;
     break;
    case 23:/* andi */
+    if(number[current_number + 1] > 31 || number[current_number + 2] > 31){
+     printf("Error: Line %li. Register provided is greater than registers available.\n", current_line);
+     exit(1);
+    }
+    machine_code[i] = 35 | ((number[current_number] & 0x1F) << 6) | (0x7 << 11) | ((number[current_number + 1] & 0x1F) << 14) | ((number[current_number + 2] & 0xFFF) << 19);
+    current_number += 3;
+    current_instruction++;
     break;
    case 24:/* slli */
+    if(number[current_number + 1] > 31 || number[current_number + 2] > 31){
+     printf("Error: Line %li. Register provided is greater than registers available.\n", current_line);
+     exit(1);
+    }
+    machine_code[i] = 35 | ((number[current_number] & 0x1F) << 6) | (0x1 << 11) | ((number[current_number + 1] & 0x1F) << 14) | ((number[current_number + 2] & 0x1F) << 19) | (0x0 << 24);
+    current_number += 3;
+    current_instruction++;
     break;
    case 25:/* srli */
+    if(number[current_number + 1] > 31 || number[current_number + 2] > 31){
+     printf("Error: Line %li. Register provided is greater than registers available.\n", current_line);
+     exit(1);
+    }
+    machine_code[i] = 35 | ((number[current_number] & 0x1F) << 6) | (0x5 << 11) | ((number[current_number + 1] & 0x1F) << 14) | ((number[current_number + 2] & 0x1F) << 19) | (0x0 << 24);
+    current_number += 3;
+    current_instruction++;
     break;
    case 26:/* srai */
+    if(number[current_number + 1] > 31 || number[current_number + 2] > 31){
+     printf("Error: Line %li. Register provided is greater than registers available.\n", current_line);
+     exit(1);
+    }
+    machine_code[i] = 35 | ((number[current_number] & 0x1F) << 6) | (0x5 << 11) | ((number[current_number + 1] & 0x1F) << 14) | ((number[current_number + 2] & 0x1F) << 19) | (0x20 << 24);
+    current_number += 3;
+    current_instruction++;
     break;
    case 27:/* add */
     break;
